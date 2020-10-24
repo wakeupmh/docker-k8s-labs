944be86c35ccb29a04f86ccfb1271ff63c402a6a
wakeupmh <wakeupmh@gmail.com>
2020-10-24 15:23:47 -0300

    feature(node-express): add dcokerfile

--- nodejs-dockerized/index.js
+++ nodejs-dockerized/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send({
+app.get('/', (req, res) => res.send([
+  {
     name: 'Dylan',
     email: 'dylan@example.com'
-}))
+  }
+]))
 app.listen(port, () => console.log(`Magic Happens on port ${port}`))
