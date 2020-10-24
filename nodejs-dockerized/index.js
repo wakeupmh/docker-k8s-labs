f6feda34b97dc56536f0472465983aff740142d0
wakeupmh <wakeupmh@gmail.com>
2020-10-24 14:57:25 -0300

    feature(node-express): add nodejs and express to dockerize it

---
+++ nodejs-dockerized/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send({
+    name: 'Dylan',
+    email: 'dylan@example.com'
+}))
+app.listen(port, () => console.log(`Magic Happens on port ${port}`))
